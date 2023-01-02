c80f543426fe60eedfe3b218e92d4c160841f7aa
Paul <mothcar@gmail.com>
2023-01-02 17:26:17 +0000

    new way

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,61 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  // head: {
-  //   title: 'example-nuxtjs',
-  //   meta: [
-  //     { charset: 'utf-8' },
-  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  //     { hid: 'description', name: 'description', content: '' },
-  //   ],
-  //   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  // },
+  head: {
+    title: 'example-nuxtjs',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
 
-  // // Global CSS: https://go.nuxtjs.dev/config-css
-  // css: [],
+  serverMiddleware: [{ path: '/api', handlerer: '~/api/index.js' }],
 
-  // // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  // plugins: [],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
 
-  // // Auto import components: https://go.nuxtjs.dev/config-components
-  // components: true,
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-  // // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  // buildModules: [
-  //   // https://go.nuxtjs.dev/eslint
-  //   '@nuxtjs/eslint-module',
-  //   // https://go.nuxtjs.dev/tailwindcss
-  //   '@nuxtjs/tailwindcss',
-  // ],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-  // // Modules: https://go.nuxtjs.dev/config-modules
-  // modules: [
-  //   // https://go.nuxtjs.dev/axios
-  //   '@nuxtjs/axios',
-  //   // https://go.nuxtjs.dev/pwa
-  //   '@nuxtjs/pwa',
-  //   // https://go.nuxtjs.dev/content
-  //   '@nuxt/content',
-  // ],
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
 
-  // // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  // axios: {},
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
 
-  // // PWA module configuration: https://go.nuxtjs.dev/pwa
-  // pwa: {
-  //   manifest: {
-  //     lang: 'en',
-  //   },
-  // },
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
 
-  // // Content module configuration: https://go.nuxtjs.dev/config-content
-  // content: {},
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
 
-  // // Build Configuration: https://go.nuxtjs.dev/config-build
-  // build: {},
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
 
-  // server: {
-  //   host: '0.0.0.0',
-  // },
-  runtimeConfig: {
-    baseUrl:
-      'https://port-0-panzer-nuxtjs-9z2ygb26lcez94uj.gksl2.cloudtype.app/api/',
-    appSecret: '',
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  server: {
+    host: '0.0.0.0',
   },
 }
